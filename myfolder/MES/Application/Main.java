d09601ac825685927de48d8123434fea807aded5
Pitsurani <84747075+Pitsurani@users.noreply.github.com>
2024-04-22 09:06:57 +0100

    Add files via upload

---
+++ myfolder/MES/Application/Main.java
@@ -0,0 +1,13 @@
+package Application;
+
+import service.OpcuaClient;
+import service.TcpClient;
+import service.jsonServices;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.ArrayList;
+
+
+public class Main {
+}
