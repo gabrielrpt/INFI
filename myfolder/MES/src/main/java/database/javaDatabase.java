ea5a299fe448a937e71f5a7394fa85aeb72aa2ae
gabrielrpt <167464190+gabrielrpt@users.noreply.github.com>
2024-05-01 19:35:56 +0100

    update

---
+++ myfolder/MES/src/main/java/database/javaDatabase.java
@@ -0,0 +1,121 @@
+package database;
+
+import java.sql.*;
+
+public class javaDatabase {
+
+    static Statement stmt;
+    static ResultSet rs;
+    static Connection connection;
+    static String databaseUrl = "jdbc:postgresql://db.fe.up.pt:5432/infind202419";
+    static String user = "infind202419";
+    static String password = "m6Fhd32pLt";
+    static String ordersTable = "orders";
+    static String piecesTable = "pieces";
+
+
+    public static void databaseConnection(String databaseUrl, String user, String password) throws SQLException {
+        try {
+            connection = DriverManager.getConnection(databaseUrl, user, password);
+
+        } catch (SQLException e) {
+            throw e;
+        }
+    }
+
+    public boolean isConnected() throws SQLException {
+        try (Connection connection = DriverManager.getConnection(databaseUrl, user, password)) {
+            return true; // Connection established if no exception
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+
+    public static int newEntry(String SQLQuery, String databaseUrl, String user, String password) throws SQLException {
+
+        databaseConnection(databaseUrl,user,password);
+
+        try{
+            stmt = connection.createStatement();
+            int i = stmt.executeUpdate(SQLQuery);
+            connection.close();
+            return i;
+
+        }catch(Exception e){
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+            return -1;
+        }
+    }
+
+    // Create method insertOrder
+    public static int insertOrder(String orderNumber, String workPiece, int quantity, int dueDate, double latePenalty, double earlyPenalty) throws SQLException {
+        String SQLQuery = "INSERT INTO erpmes." + ordersTable + " (ordernumber, workpiece, quantity, duedate, latepen, earlypen) VALUES ('" + orderNumber + "', '" + workPiece + "', " + quantity + ", " + dueDate + ", " + latePenalty + ", " + earlyPenalty + ");";
+        System.out.println(SQLQuery);
+        return newEntry(SQLQuery, databaseUrl, user, password);
+    }
+
+    public static int insertOrderCost(double orderCost) throws SQLException {
+        String SQLQuery = "UPDATE erpmes." + ordersTable + " SET ordercost = "+ orderCost +" WHERE ordernumber = '124';";
+        return newEntry(SQLQuery, databaseUrl, user, password);
+    }
+
+    public static int insertPiece(String pieceName, String rawPiece, int orderNumber, double rawCost) throws SQLException {
+        String SQLQuery = "INSERT INTO erpmes." + piecesTable + " (piecetype, rawpiece, orderid, currenttype, rawcost) VALUES ('" + pieceName + "', '" + rawPiece + "', " + orderNumber + ", '" + rawPiece + "', " + rawCost + ");";
+        return newEntry(SQLQuery, databaseUrl, user, password);
+    }
+
+    public static ResultSet getPieceByOrderNumber(String orderNumber) {
+        ResultSet resultSet = null;
+        try {
+            Connection connection = DriverManager.getConnection(databaseUrl, user, password);
+            Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            String SQLQuery = "SELECT * FROM erpmes.pieces WHERE orderid = '" + orderNumber;
+            resultSet = statement.executeQuery(SQLQuery);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return resultSet;
+    }
+
+    public static ResultSet getOrderByProdDay(int productionDay) {
+        ResultSet resultSet = null;
+        try {
+            Connection connection = DriverManager.getConnection(databaseUrl, user, password);
+            Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            String SQLQuery = "SELECT * FROM erpmes.orders WHERE productionday = " + productionDay + ";";
+            resultSet = statement.executeQuery(SQLQuery);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return resultSet;
+    }
+
+
+        public static int getResultSetSize(ResultSet resultSet) {
+        int size = 0;
+        try {
+            resultSet.last();
+            size = resultSet.getRow();
+            resultSet.beforeFirst();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return size;
+    }
+    public static String getUser() {
+        return user;
+    }
+
+    public static String getDatabaseUrl() {
+        return databaseUrl;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
+    public static String getTable() {
+        return ordersTable;
+    }
+}
---
+++ myfolder/MES/src/main/java/org/OPC_UA/OpcuaClient.java
@@ -0,0 +1,93 @@
+package org.OPC_UA;
+
+import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
+import org.eclipse.milo.opcua.stack.core.types.builtin.*;
+import org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.UShort;
+import org.eclipse.milo.opcua.stack.core.types.enumerated.TimestampsToReturn;
+
+public class OpcuaClient {
+
+    private OpcUaClient myclient;
+    private double maxAge = 0;
+
+    public void connect(String endpointURL){
+        try{
+            myclient = OpcUaClient.create(endpointURL);
+            myclient.connect().get();
+            System.out.println("Connected to OPCUA server with endpoint:"+endpointURL);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public DataValue read(String variable, int index){
+        String ID = variable;
+        NodeId nodeId = new NodeId(index, ID);
+        DataValue value;
+        try {
+            value = myclient.readValue(maxAge, TimestampsToReturn.Both, nodeId).get();
+            return value;
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public boolean writeWord(String variable, int index, String value) {
+        UShort uShort = UShort.valueOf(value);
+        String ID =  variable;
+        NodeId nodeId = new NodeId(index, ID);
+        Variant variant = new Variant(uShort);
+        DataValue dataValue = new DataValue(variant);
+
+        try{
+            myclient.writeValue(nodeId,dataValue).get();
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean writeInt16(String variable, int index, String value) {
+        Short shortValue = Short.valueOf(value);
+        String ID = variable;
+        NodeId nodeId = new NodeId(index, ID);
+        Variant variant = new Variant(shortValue);
+        DataValue dataValue = new DataValue(variant);
+
+        try{
+            myclient.writeValue(nodeId,dataValue).get();
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        OpcuaClient client = new OpcuaClient();
+        client.connect("opc.tcp://localhost:4840");  // replace with your OPC UA server's endpoint URL
+        DataValue value = client.read("|var|CODESYS Control Win V3 x64.Application.PLC_PRG.C10.offset_O", 4);
+
+        // Now you can get the value and its type
+        Object val = value.getValue().getValue();
+        System.out.println("Value: " + val);
+
+        //boolean resultWord = client.writeWord("|var|CODESYS Control Win V3 x64.Application.GVL.raulnengue", 4, "10");
+        boolean resultInt = client.writeInt16("|var|CODESYS Control Win V3 x64.Application.GVL.raulnengue", 4, "8");
+
+
+
+        if (resultInt) {
+            System.out.println("Integer value written successfully.");
+        } else {
+            System.out.println("Failed to write integer value.");
+        }
+
+
+    }
+    }
