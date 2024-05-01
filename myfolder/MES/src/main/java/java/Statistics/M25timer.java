83907f5ed0ed196ff833627ebf7a7d175c53ca45
Pitsurani <84747075+Pitsurani@users.noreply.github.com>
2024-05-01 23:30:55 +0100

    Add files via upload

---
+++ myfolder/MES/src/main/java/java/Statistics/M25timer.java
@@ -0,0 +1,29 @@
+package Statistics;
+
+import org.OPC_UA.OpcuaClient;
+import org.eclipse.milo.opcua.stack.core.types.builtin.DataValue;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class M25timer {
+    public void M25_timer() {
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask(){
+            int time=0;
+
+
+            @Override
+            public void run() {
+                DataValue data = OpcuaClient.read("|var|CODESYS Control Win V3 x64.Application.PLC_PRG.M25.Process.x", 4);
+                if (data.getValue().getValue() instanceof Boolean && (Boolean)data.getValue().getValue()){
+                    time++;
+                }
+
+            }
+
+        };
+        timer.scheduleAtFixedRate(task, 0, 1000);
+    }
+}
+
