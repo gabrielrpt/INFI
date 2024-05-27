7253e8bb2560869864dde733336555179a889302
gabrielrpt <167464190+gabrielrpt@users.noreply.github.com>
2024-05-27 23:51:40 +0100

    Double Window Fix
    Double Window Fix in Machine Statistics

--- myfolder/MES/src/main/java/GUI/MainMenu.java
+++ myfolder/MES/src/main/java/GUI/MainMenu.java
@@ -14,7 +14,7 @@ import java.util.GregorianCalendar;
  */
 public class MainMenu extends javax.swing.JFrame {
     public OrderStatus OrderStatus = new OrderStatus();
-
+    MachineStatistics machinestatistics= new MachineStatistics();
     /**
      * Creates new form MainMenu
      */
@@ -129,7 +129,7 @@ public class MainMenu extends javax.swing.JFrame {
         unloadingdocks.setVisible(true);    }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        MachineStatistics machinestatistics= new MachineStatistics();
+       // MachineStatistics machinestatistics= new MachineStatistics();
         machinestatistics.setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
